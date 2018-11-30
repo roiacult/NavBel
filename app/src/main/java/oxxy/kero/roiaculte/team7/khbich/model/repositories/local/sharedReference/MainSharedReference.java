11a40a363a7946ac265d567e73dcf1721a4674fd
akram09 <a.boutouchent@esi-sba.dz>
2018-11-30 01:59:39 +0100

    Signout configured

--- app/src/main/java/oxxy/kero/roiaculte/team7/khbich/model/repositories/local/sharedReference/MainSharedReference.java
+++ app/src/main/java/oxxy/kero/roiaculte/team7/khbich/model/repositories/local/sharedReference/MainSharedReference.java
@@ -69,5 +69,15 @@ public class MainSharedReference {
    public void signUserOut(){
         SharedPreferences.Editor editor= preferences.edit();
         editor.putBoolean(IS_USER_LOGGED, false);
+       editor.remove(USER_PASSWORD);
+       editor.remove(USER_NAME);
+       editor.remove(USER_MAIL);
+       editor.remove(USER_PICTURE);
+       editor.remove(USER_YEAR);
+       editor.remove(USER_POINT);
+       editor.remove(QSOLVED);
+       editor.remove(LEVEL);
+       editor.remove(Date);
+       editor.commit();
    }
         }
